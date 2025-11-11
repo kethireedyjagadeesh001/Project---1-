ebcc10bcf0cbc3b57ed84ec648f56c4a4af6f52b
kethireedyjagadeesh001 <jkethireedy@gmail.com>
2025-11-11 18:00:20 +0530

    Add files via upload

---
+++ postive or negative numbers.js
@@ -0,0 +1,30 @@
+
+
+  // It is postive or negative number
+
+
+ let a=20;
+
+      if(a>0){
+        console.log(a , "is a postive");                // By using if and if else statment only
+      }
+       else if(a<0){
+        console.log(a , "is a negative");
+       }
+        else{
+            console.log(a , "is a zero");
+        }
+
+     console.log("-----------------------");
+
+    //  It is odd Or even  number
+
+
+        let b=112;
+        if(b%2===0){
+         console.log( b , "even");
+        }else{
+          console.log( b , "odd");
+        }
+
+        console.log("-----------------------");
